78b33d60a093322fdbb88e5a881d90884ca2e205
SiiR-Affinity <matt.mcg89@gmail.com>
2022-08-16 15:26:21 +0100

    create global js for links

---
+++ src/data/Links.js
@@ -0,0 +1,7 @@
+export const links = {
+	appleStore: 'https://apps.apple.com/us/app/aim-lab-mobile/id1616550573',
+	googleStore:
+		'https://play.google.com/store/apps/details?id=gg.statespace.aimlab',
+};
+
+export default links;
