f8ae9d0fa2a4f08b21c4debb142cda00568b2a92
SiiR-Affinity <matt.mcg89@gmail.com>
2022-08-16 15:26:30 +0100

    update exports

--- src/images/index.js
+++ src/images/index.js
@@ -1,4 +1,4 @@
-export const headerImages = {
+export const images = {
 	logo: require('./al_logo_mobile_white.png'),
 	gbadge: require('./google_badge_cta.png'),
 	abadge: require('./apple_badge_cta.png'),
@@ -6,5 +6,7 @@ export const headerImages = {
 
 export const carouselImages = {
 	slide2: require('./presets_5x9.png'),
-	slide3: require('./profile_5x9.png'),
+	slide3: require('./timed_events_16x9.png'),
+	slide4: require('./profile_5x9.png'),
+	slide5: require('./gridshot_5x9.png'),
 };
