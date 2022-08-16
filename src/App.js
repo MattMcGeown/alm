12af34b25ca15c9b05165c507ebaffecf7298b17
SiiR-Affinity <matt.mcg89@gmail.com>
2022-08-16 15:24:02 +0100

    update imports, add section title, add footer

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './scss/main.scss';
 import './app.scss';
 
 // Data Imports
-import { carouselData } from './components/Carousel/CarouselData';
+import { carouselData } from './data/CarouselData';
 import { faqData } from './data/FAQ';
 
 // Component Imports
 import Header from './components/Header/Header';
 import Carousel from './components/Carousel/Carousel';
 import Accordion from './components/Accordion/Accordion';
+import Footer from './components/Footer/Footer';
 
 function App() {
 	return (
@@ -19,7 +20,10 @@ function App() {
 				<Carousel slides={carouselData} />
 			</section>
 			<section className='faq'>
-				<Accordion title='FAQ' data={faqData} />
+				<Accordion title='Frequently Asked Questions' data={faqData} />
+			</section>
+			<section className='footer'>
+				<Footer title='Play for free' />
 			</section>
 		</div>
 	);
