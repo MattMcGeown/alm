1c73565d3ce005138246d65331d1a26dc8f113db
SiiR-Affinity <matt.mcg89@gmail.com>
2022-08-17 19:08:58 +0100

    add: top level container

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -16,40 +16,42 @@ const Accordion = ({ title, data }) => {
 	};
 
 	return (
-		<div className='container'>
-			<h2 className='section_title'>{title}</h2>
-			<div className='accordion_container'>
-				{data.map((item, index) => {
-					return (
-						<div className='accordion_item' key={index}>
-							<div
-								className='accordion_item_title'
-								onClick={() => handleClick(index)}>
-								{item.question}
+		<div className='accordion'>
+			<div className='container'>
+				<h2 className='section_title'>{title}</h2>
+				<div className='accordion_container'>
+					{data.map((item, index) => {
+						return (
+							<div className='accordion_item' key={index}>
+								<div
+									className='accordion_item_title'
+									onClick={() => handleClick(index)}>
+									{item.question}
+									<motion.div
+										initial={{ rotate: 0 }}
+										animate={{ rotate: selectedItem === index ? 180 : 0 }}
+										className='chevron'>
+										<FaChevronDown />
+									</motion.div>
+								</div>
 								<motion.div
-									initial={{ rotate: 0 }}
-									animate={{ rotate: selectedItem === index ? 180 : 0 }}
-									className='chevron'>
-									<FaChevronDown />
-								</motion.div>
-							</div>
-							<motion.div
-								initial={{ height: 0 }}
-								animate={{
-									height: selectedItem === index ? 'auto' : 0,
-								}}
-								transition={{
-									duration: 0.5,
-								}}
-								className='accordion_item_content'>
-								{/* {item.answer.map((line) => {
+									initial={{ height: 0 }}
+									animate={{
+										height: selectedItem === index ? 'auto' : 0,
+									}}
+									transition={{
+										duration: 0.5,
+									}}
+									className='accordion_item_content'>
+									{/* {item.answer.map((line) => {
 									return <p>{line}</p>;
 								})} */}
-								<p>{item.answer}</p>
-							</motion.div>
-						</div>
-					);
-				})}
+									<p>{item.answer}</p>
+								</motion.div>
+							</div>
+						);
+					})}
+				</div>
 			</div>
 		</div>
 	);
