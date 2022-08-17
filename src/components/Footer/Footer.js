8294a7077626aa45333e9fdb0a2992a326a4d575
Matt <matt_live@live.co.uk>
2022-08-17 19:20:05 +0100

    add: top level container

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,31 +10,39 @@ import Signup from '../Signup/Signup';
 
 const footer = ({ title }) => {
 	return (
-		<footer className='container'>
-			<h2 className='section_title'>{title}</h2>
-			<div className='footer_content'>
-				<div className='store_btn_wrap'>
-					<a href={links.googleStore} target='_blank' rel='noopener noreferrer'>
-						<img src={images.gbadge} alt='' />
-					</a>
-					<a href={links.appleStore} target='_blank' rel='noopener noreferrer'>
-						<img src={images.abadge} alt='' />
-					</a>
-				</div>
-				<Signup />
-				<div className='social_link_container'>
-					{socialLinks.map((link, index) => {
-						return (
-							<a
-								style={{ order: link.position }}
-								href={link.url}
-								key={index}
-								target='_blank'
-								rel='noopener noreferrer'>
-								{link.icon ? link.icon : link.label}
-							</a>
-						);
-					})}
+		<footer>
+			<div className='container'>
+				<h2 className='section_title'>{title}</h2>
+				<div className='footer_content'>
+					<div className='store_btn_wrap'>
+						<a
+							href={links.googleStore}
+							target='_blank'
+							rel='noopener noreferrer'>
+							<img src={images.gbadge} alt='' />
+						</a>
+						<a
+							href={links.appleStore}
+							target='_blank'
+							rel='noopener noreferrer'>
+							<img src={images.abadge} alt='' />
+						</a>
+					</div>
+					<Signup />
+					<div className='social_link_container'>
+						{socialLinks.map((link, index) => {
+							return (
+								<a
+									style={{ order: link.position }}
+									href={link.url}
+									key={index}
+									target='_blank'
+									rel='noopener noreferrer'>
+									{link.icon ? link.icon : link.label}
+								</a>
+							);
+						})}
+					</div>
 				</div>
 			</div>
 		</footer>
